90675cbbb8cfa4143e0ab14ae02bb4e50ee72542
voytkonick <voytko.nick@gmail.com>
2023-07-18 20:30:09 +0300

    add Example005_IfElse update error

--- Example005_IfElse/Program.cs
+++ Example005_IfElse/Program.cs
@@ -1,13 +1,13 @@
 ﻿Console.Write("Введите имя пользователя: ");
-string username = Console.RedLine();
+string username = Console.ReadLine();
 
-if(username == "Маша");
-{ 
-    Console.WriteLine("Ура, это же Маша!")
+if(username == "Маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
 }
 else
 {
     Console.Write("Привет, ");
-    Comsole.WriteLine(username);
+    Console.WriteLine(username);
 
 }
