38157ea018d53e7b4cfc539b4a630e9eb14cac07
voytkonick <voytko.nick@gmail.com>
2023-07-18 20:25:25 +0300

    add Example005_IfElse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.RedLine();
+
+if(username == "Маша");
+{ 
+    Console.WriteLine("Ура, это же Маша!")
+}
+else
+{
+    Console.Write("Привет, ");
+    Comsole.WriteLine(username);
+
+}
